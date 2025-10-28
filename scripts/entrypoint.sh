0df5c8ddbf1e4c3b5648636c1862a71d8a771466
Andrzej Pultyn <andrzej.pultyn.stud@pw.edu.pl>
2025-10-28 17:20:50 +0100

    Removed redundant assignment

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Database parameters
-DB_HOST=${DB_HOST}
-DB_USER=${DB_USERNAME}
-DB_PASSWORD=${DB_PASSWORD}
-
 echo "Waiting for database to be ready..."
 
 # Check database status
-while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
+while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" --silent; do
     echo "Database is not ready yet. Retrying in 5 seconds..."
     sleep 5
 done
